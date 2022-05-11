a7287adc2f6b56f5e2e31a9ad48f120082272dee
Gleb V. Zhegulin <gleboceanborn@gmail.com>
2022-05-11 03:01:10 +0300

    version use openweathermap

--- main.rb
+++ main.rb
@@ -1,32 +1,32 @@
 require_relative 'lib/wardrobe'
-require 'pry-byebug'
+require 'rack'
+require 'open-uri'
+require 'json'
+require 'uri'
+require 'addressable'
+
+WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather/'.freeze
+WEATHER_APP_ID = '643b38c496bbf3acae79cdc01c81c36c'.freeze
+url = WEATHER_API_URL
+
+query = Rack::Utils.build_query(
+  'q' => 'Saint Petersburg',
+  'APPID' => WEATHER_APP_ID,
+  'units' => 'metric'
+)
+
+# response = URI.open("#{url}?#{query}").read
+response = URI.open(Addressable::URI.encode("#{url}?#{query}")).read
+json = JSON.parse(response)
+current_temperature = json['main']['temp'].round.to_i
+puts "Текущая температура: #{current_temperature} C"
 
 clothes_collection = Wardrobe.read_from_dir_files("#{__dir__}/data/*.txt")
-temperature = nil
 
-until temperature =~ /-?\d+/
-  puts 'Сколько градусов на улице?'
-  temperature = $stdin.gets.strip
-end
-
-temperature = temperature.to_i
-
-# temperature = $stdin.gets.strip
-# unless temperature.match? /-?\d+/
-#   puts 'Сколько градусов на улице?'
-#   temperature = STDIN.gets.strip
-# end
-
-clothes_list = clothes_collection.clothes_by_weather(temperature)
+clothes_list = clothes_collection.clothes_by_weather(current_temperature)
 
 if clothes_list.empty?
   puts 'У вас нет подходящей одежды'
 else
   puts 'Предлагаю надеть:', clothes_list
 end
-
-# puts <<~LIST
-#   \nСписок инстанс-методов класса Wardrobe"
-#   #{Wardrobe.class.instance_methods.grep(/\?/)}\n
-#   #{Wardrobe.singleton_class?}
-# LIST
